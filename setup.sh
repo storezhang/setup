d747bec49f45bf1ad3be220f19b232f06fcf6f1b
storezhang <storezhang@gmail.com>
2021-09-14 14:07:03 +0800

    项目初始化

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USERNAME=storezhang
+NEED_LOGOUT=false
 
 # 取得Root权限
 sudo -i
@@ -14,6 +15,9 @@ else
 
     echo "将用户添加到Sudo组"
     usermod -aG sudo ${USERNAME}
+
+    # 需要重启使docker命令立即生效
+    NEED_LOGOUT=true
 fi
 
 
@@ -22,52 +26,73 @@ apt update
 apt upgrade
 
 
-echo "安装Docker"
-apt install docker.io
-usermod -aG docker ${USERNAME}
+DOCKER_APP="docker.io"
+if ! apt list | grep -q "${DOCKER_APP}"; then
+  echo "安装Docker"
+  apt install ${DOCKER_APP}
+  usermod -aG docker ${USERNAME}
 
-echo "写入镜像地址"
-cat>/etc/docker/daemon.json<<EOF
-{
-        "registry-mirrors": [
-                "https://docker.mirrors.ustc.edu.cn"
-        ]
-}
+  echo "写入镜像地址"
+  cat>/etc/docker/daemon.json<<EOF
+  {
+          "registry-mirrors": [
+                  "https://docker.mirrors.ustc.edu.cn"
+          ]
+  }
 EOF
+fi
 
 
-echo "完全删阶Snap"
-snap remove $(snap list | awk '!/^Name|^core/ {print $1}')
-umount /var/snap
-systemctl stop snapd
-apt remove --purge --assume-yes snapd gnome-software-plugin-snap
-rm -rf ~/snap
-rm -rf /snap
-rm -rf /var/snap
-rm -rf /var/lib/snapd
+SNAP_APP="snapd"
+if ! apt list | grep -q "${SNAP_APP}"; then
+  echo "完全删除Snap"
+  snap remove --purge "$(snap list | awk '!/^Name|^core/ {print $1}')"
+  umount /var/snap
+  systemctl stop snapd
+  apt remove --purge --assume-yes ${SNAP_APP} gnome-software-plugin-snap
+  rm -rf ~/snap
+  rm -rf /snap
+  rm -rf /var/snap
+  rm -rf /var/lib/snapd
+fi
 
 
 echo "增加计划任务"
-echo "自动关机"
-(crontab -l ; echo "") | crontab -
-(crontab -l ; echo "# 自动关机") | crontab -
-(crontab -l ; echo "59	00	*	*	*	/sbin/shutdown -h now") | crontab -
 
-echo "自动更新系统"
-(crontab -l ; echo "") | crontab -
-(crontab -l ; echo "# 自动更新系统") | crontab -
-(crontab -l ; echo "00	09	*	*	*	apt update -y && apt upgrade -y") | crontab -
+CRON_TASK="自动关机"
+if ! crontab -l | grep -q "${CRON_TASK}"; then
+  echo "添加任务${CRON_TASK}"
+  (crontab -l ; echo "") | crontab -
+  (crontab -l ; echo "# ${CRON_TASK}") | crontab -
+  (crontab -l ; echo "59	00	*	*	*	/sbin/shutdown -h now") | crontab -
+fi
 
-echo "清理Docker日志"
-(crontab -l ; echo "") | crontab -
-(crontab -l ; echo "# 清理Docker日志") | crontab -
-(crontab -l ; echo "30	09	*	*	1	docker ps | awk '{if (NR>1){print $1}}' | xargs docker inspect --format='{{.LogPath}}' | xargs truncate -s 0") | crontab -
+CRON_TASK="自动更新系统"
+if ! crontab -l | grep -q "${CRON_TASK}"; then
+  echo "添加任务${CRON_TASK}"
+  (crontab -l ; echo "") | crontab -
+  (crontab -l ; echo "# ${CRON_TASK}") | crontab -
+  (crontab -l ; echo "00	09	*	*	*	apt update -y && apt upgrade -y") | crontab -
+fi
 
-echo "清理Docker"
-(crontab -l ; echo "") | crontab -
-(crontab -l ; echo "# 清理Docker") | crontab -
-(crontab -l ; echo "30	08	*	*	*	docker system prune --all --force --volumes") | crontab -
+CRON_TASK="清理Docker日志"
+if ! crontab -l | grep -q "${CRON_TASK}"; then
+  echo "添加任务${CRON_TASK}"
+  (crontab -l ; echo "") | crontab -
+  (crontab -l ; echo "# ${CRON_TASK}") | crontab -
+  (crontab -l ; echo "30	09	*	*	1	docker ps | awk '{if (NR>1){print $1}}' | xargs docker inspect --format='{{.LogPath}}' | xargs truncate -s 0") | crontab -
+fi
+
+CRON_TASK="清理Docker"
+if ! crontab -l | grep -q "${CRON_TASK}"; then
+  echo "添加任务${CRON_TASK}"
+  (crontab -l ; echo "") | crontab -
+  (crontab -l ; echo "# ${CRON_TASK}") | crontab -
+  (crontab -l ; echo "30	08	*	*	*	docker system prune --all --force --volumes") | crontab -
+fi
 
 
-echo "注销当前Shell，使配置生效"
-logout
+if [ "${NEED_LOGOUT}" = true ] ; then
+  echo "注销当前Shell，使配置生效"
+  logout
+fi
