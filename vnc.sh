3b688221dde214dbd6a01846ef862eabd0120fef
张川 <storezhang@gmail.com>
2024-11-04 15:05:42 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-password="123456"  # 这里请改成你自己登录账号的密码就可以，下方内容都不用变
+password="$1"
 
-# Step 1: 安装x11vnc
+# 安装
 echo $password | sudo -S sudo apt update && sudo apt install x11vnc -y
 
-# Step 2: 设置x11vnc密码
-sudo x11vnc -storepasswd $password /etc/x11vnc.pass
+# 设置密码
+sudo x11vnc -storepasswd $password /etc/vnc/passwd.pass
 
-# Step 3: 创建服务
-sudo tee /etc/systemd/system/x11vnc.service > /dev/null <<EOF
+# 创建服务
+sudo tee /etc/systemd/system/vnc.service > /dev/null <<EOF
 [Unit]
 Description=Remote desktop service (VNC)
 Requires=display-manager.service
@@ -17,7 +17,7 @@ After=display-manager.service
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/x11vnc -auth guess -forever -rfbauth /etc/x11vnc.pass -rfbport 5900 -shared
+ExecStart=/usr/bin/x11vnc -auth guess -forever -rfbauth /etc/vnc/passwd.pass -rfbport 5900 -shared
 ExecStop=/usr/bin/killall x11vnc
 
 [Install]
@@ -29,8 +29,8 @@ echo "服务文件写入完成！"
 sudo systemctl daemon-reload
 
 # 启动服务
-sudo systemctl start x11vnc
+sudo systemctl start vnc
 
 # 设置服务为自启动
-sudo systemctl enable x11vnc
+sudo systemctl enable vnc
 echo "服务启动设置成功！"
