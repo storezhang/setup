b533d0717a597a4d210a10d531babfbf81d62bbb
张川 <storezhang@gmail.com>
2024-11-14 15:51:18 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -19,28 +19,14 @@ sudo mkdir -p /etc/vnc
 sudo x11vnc -storepasswd $password /etc/vnc/passwd.pass
 
 # 创建服务
-sudo tee /etc/systemd/system/vnc.service > /dev/null <<EOF
-[Unit]
-Description=VNC
-Requires=display-manager.service
-After=display-manager.service
+sudo tee /etc/init/vnc.conf > /dev/null <<EOF
+start on login-session-start
 
-[Service]
-Type=simple
-ExecStart=/usr/bin/x11vnc -auth guess -forever -rfbauth /etc/vnc/passwd.pass -rfbport 5900 -shared
-ExecStop=/usr/bin/killall x11vnc
+script 
 
-[Install]
-WantedBy=multi-user.target
+x11vnc -display :0 -auth /var/run/lightdm/root/:0 -forever -bg -o /var/log/vnc.log -rfbauth /etc/vnc/passwd.pass -rfbport 5900 
+
+end script
 EOF
 
 echo "服务文件写入完成！"
-# 刷新服务配置
-sudo systemctl daemon-reload
-
-# 启动服务
-sudo systemctl start vnc
-
-# 设置服务为自启动
-sudo systemctl enable vnc
-echo "服务启动设置成功！"
